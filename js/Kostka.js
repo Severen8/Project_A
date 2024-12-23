1c8e8f8675b318418a70357f28626cf8b4257973
mountain-bit <kamiljek7@gmail.com>
2024-12-23 17:31:34 +0100

    refaktoryzacja klasy Kostka

--- js/Kostka.js
+++ js/Kostka.js
@@ -1,12 +1,29 @@
+function isNumber(liczba){
+     if (typeof liczba!== 'number') return false;
+     return true;
+}
+
+
+
 class Kostka {
+    #iloscScian;
+    #modyfikator;
     constructor(iloscScian, modyfikator=0) {
-        if (typeof iloscScian !== 'number' || iloscScian <= 0) {
+        if (!isNumber(iloscScian) || iloscScian <= 0) {
             throw new Error("Liczba ścian musi być liczbą dodatnią.");
         }
-        this.iloscScian = iloscScian;
-        this.modyfikator = modyfikator === undefined ? 0 : modyfikator;
+        this.#iloscScian = iloscScian;
+        if(isNumber(modyfikator)) this.#modyfikator = modyfikator;
+        else this.#modyfikator = 0;
     }
 
+    getIloscScian(){
+        return this.#iloscScian;
+    }
+
+    getModyfikator(){
+        return this.#modyfikator;
+    }
 
     toString() {
       return `Kostka d${this.iloscScian}${this.modyfikator > 0 ? '+' + this.modyfikator : this.modyfikator < 0 ? this.modyfikator : ''}`;
@@ -14,9 +31,12 @@ class Kostka {
 }
 
 function stworzKostke(iloscScian, modyfikator) {
-
+    if (!isNumber(iloscScian) || iloscScian <= 0) {
+        return null;
+    }
+    if(!isNumber(modyfikator))  modyfikator=0;
     return new Kostka(iloscScian, modyfikator);
-  }
+}
 
-
+module.exports = Kostka
 module.exports = stworzKostke;
