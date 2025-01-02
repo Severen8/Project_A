8b9d113c3f2b09928ec5e47bc199442fcae2de98
Severen8 <s61309@student.ubb.edu.pl>
2025-01-02 15:23:34 +0100

    Modyfikacja Klas Item oraz Weapon

--- js/Item.js
+++ js/Item.js
@@ -1,25 +1,35 @@
-export class Przedmiot {
-    constructor(itemID, itemName, itemType, itemAttributes, itemDesc) {
-        this.itemID = itemID;
-        this.itemName = itemName;
-        this.itemType = itemType;
-        this.itemAttributes = itemAttributes;
-        this.itemDesc = itemDesc;
+(function (global, factory) {
+    if (typeof module !== "undefined" && module.exports) {
+        //(Gitbash)
+        module.exports = factory();
+    } else {
+        //(Przegladarka)
+        global.Przedmiot = factory();
     }
-
-    static create({itemID, itemName, itemType, itemAttributes, itemDesc}) {
-        if (!itemID || !itemName || !itemType || !itemAttributes) {
-            throw new Error("Błąd podczas tworzenia Przedmiotu -> nieodpowiednio wypełnione wszystkie parametry");
+})(this, function () {
+    class Przedmiot {
+        constructor(itemID, itemName, itemType, itemAttributes, itemDesc) {
+            this.itemID = itemID;
+            this.itemName = itemName;
+            this.itemType = itemType;
+            this.itemAttributes = itemAttributes;
+            this.itemDesc = itemDesc;
         }
-
-        if (!Array.isArray(itemAttributes)) {
-            throw new Error("Atrybuty muszą być tablicą");
+    
+        static create({itemID, itemName, itemType, itemAttributes, itemDesc}) {
+            if (!itemID || !itemName || !itemType || !itemAttributes) {
+                throw new Error("Błąd podczas tworzenia Przedmiotu -> nieodpowiednio wypełnione wszystkie parametry");
+            }
+    
+            if (!Array.isArray(itemAttributes)) {
+                throw new Error("Atrybuty muszą być tablicą");
+            }
+    
+            return new Przedmiot(itemID, itemName, itemType, itemAttributes, itemDesc);
         }
-
-        return new Przedmiot(itemID, itemName, itemType, itemAttributes, itemDesc);
+    
+        getDescription() {
+            return `ID: ${this.itemID}, Przedmiot: ${this.itemName}, Typ: ${this.itemType}, Atrybuty: ${JSON.stringify(this.itemAttributes)}, \nDesc: ${this.itemDesc}`;
+        }
     }
-
-    getDescription() {
-        return `ID: ${this.itemID}, Przedmiot: ${this.itemName}, Typ: ${this.itemType}, Atrybuty: ${JSON.stringify(this.itemAttributes)}, \nDesc: ${this.itemDesc}`;
-    }
-}
+});
--- js/Weapon.js
+++ js/Weapon.js
@@ -1,14 +1,25 @@
-import { Przedmiot } from './Item.js';
-
-export class Bron extends Przedmiot {
-    constructor(itemID, itemName, itemType, itemAttributes, itemDesc) {
-        super(itemID, itemName, itemType, itemAttributes, itemDesc);
-
-        if (toString(this.itemType).toLowerCase() == "weapon") {
-            console.log("Broń" + this.getDescription())
-        } else {
-            console.log("Inny Przedmiot" + this.getDescription());
-        }
-        
+(function (global, factory) {
+    if (typeof module !== "undefined" && module.exports) {
+        //(Gitbash)
+        const Przedmiot = require('./Przedmiot');
+        module.exports = factory(Przedmiot);
+    } else {
+        //(Przegladarka)
+        global.Weapon = factory(global.Przedmiot);
     }
-}
+})(this, function (Przedmiot) {
+    class Bron extends Przedmiot {
+        constructor(itemID, itemName, itemType, itemAttributes, itemDesc) {
+            super(itemID, itemName, itemType, itemAttributes, itemDesc);
+    
+            if (toString(this.itemType).toLowerCase() == "weapon") {
+                console.log("Broń" + this.getDescription())
+            } else {
+                console.log("Inny Przedmiot" + this.getDescription());
+            }
+            
+        }
+    }
+
+    return Bron;
+});
