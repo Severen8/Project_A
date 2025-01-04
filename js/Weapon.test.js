8a5991988e3849aa315e418c364ec8b65e2214c5
Severen8 <s61309@student.ubb.edu.pl>
2025-01-04 16:16:14 +0100

    Utworzenie testu dla i poprawka samej klasy Weapon.js

---
+++ js/Weapon.test.js
@@ -0,0 +1,31 @@
+//Test uruchamiany w Gitbash uzywając: npx jest
+const Przedmiot = require('./Item');
+const Bron = require('./Weapon');
+
+describe('Bron', () => {
+    it('powinna dziedziczyć właściwości z klasy Przedmiot', () => {
+        const bron = new Bron(1, 'Miecz', 'Weapon', ['ostry', 'stalowy'], 'Miecz bojowy');
+
+        expect(bron.itemID).toBe(1);
+        expect(bron.itemName).toBe('Miecz');
+        expect(bron.itemType).toBe('Weapon');
+        expect(bron.itemAttributes).toEqual(['ostry', 'stalowy']);
+        expect(bron.itemDesc).toBe('Miecz bojowy');
+    });
+
+    it('powinna wyświetlić poprawny komunikat, jeśli typ to "Weapon"', () => {
+        const consoleSpy = jest.spyOn(console, 'log');
+        new Bron(2, 'Tarcza', 'Weapon', ['ciężka', 'drewniana'], 'Solidna tarcza');
+        
+        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Broń'));
+        consoleSpy.mockRestore();
+    });
+
+    it('powinna wyświetlić poprawny komunikat, jeśli typ to nie "Weapon"', () => {
+        const consoleSpy = jest.spyOn(console, 'log');
+        new Bron(3, 'Hełm', 'Armor', ['metalowy', 'wytrzymały'], 'Rycerski hełm');
+        
+        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Inny Przedmiot'));
+        consoleSpy.mockRestore();
+    });
+});
