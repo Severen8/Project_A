6e980cf76280ddc340cd60f9cc40e1909b823360
Severen8 <s61309@student.ubb.edu.pl>
2025-01-13 13:47:15 +0100

    Przedmiot, Bron i ich Testy - Aktualizacja

--- js/Item.js
+++ js/Item.js
@@ -1,14 +1,16 @@
 (function (global, factory) {
     if (typeof module !== "undefined" && module.exports) {
         //(Gitbash)
-        module.exports = factory();
+        const Cecha = require('./Trait.js');
+        module.exports = factory(Cecha);
     } else {
         //(Przegladarka)
-        global.Przedmiot = factory();
+        global.Przedmiot = factory(global.Cecha);
     }
-})(this, function () {
-    class Przedmiot {
-        constructor(itemID, itemName, itemType, itemAttributes, itemDesc) {
+})(this, function (Cecha) {
+    class Przedmiot extends Cecha {
+        constructor(itemID, itemName, itemType, itemAttributes, itemDesc, traitName, traitEffects, traitDesc) {
+            super(traitName, traitEffects, traitDesc);
             this.itemID = itemID;
             this.itemName = itemName;
             this.itemType = itemType;
@@ -16,7 +18,7 @@
             this.itemDesc = itemDesc;
         }
     
-        static create({itemID, itemName, itemType, itemAttributes, itemDesc}) {
+        static create({itemID, itemName, itemType, itemAttributes, itemDesc, traitName, traitEffects, traitDesc}) {
             if (!itemID || !itemName || !itemType || !itemAttributes) {
                 throw new Error("Błąd podczas tworzenia Przedmiotu -> nieodpowiednio wypełnione wszystkie parametry");
             }
@@ -25,11 +27,11 @@
                 throw new Error("Atrybuty muszą być tablicą");
             }
     
-            return new Przedmiot(itemID, itemName, itemType, itemAttributes, itemDesc);
+            return new Przedmiot(itemID, itemName, itemType, itemAttributes, itemDesc, traitName, traitEffects, traitDesc);
         }
     
         getDescription() {
-            return `ID: ${this.itemID}, Przedmiot: ${this.itemName}, Typ: ${this.itemType}, Atrybuty: ${JSON.stringify(this.itemAttributes)}, \nDesc: ${this.itemDesc}`;
+            return `ID: ${this.itemID}, Przedmiot: ${this.itemName}, Typ: ${this.itemType}, Atrybuty: ${JSON.stringify(this.itemAttributes)}, \nDesc: ${this.itemDesc},\nCecha: ${super.getDescription()}`;
         }
     }
 
